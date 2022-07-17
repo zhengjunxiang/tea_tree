7bf8c0de877fdb1a8ca4c40c8a1d74e9900bb277
junxiang.zheng <junxiang.zheng@matrixport.com>
2022-07-17 20:19:48 +0800

    feat: 优化选中田地方法

--- pages/field/field.js
+++ pages/field/field.js
@@ -1,7 +1,8 @@
 Page({
   data: {
-    wWidth: 0,
+    cWidth: 750, // 设计稿的宽度
     rateW: 0,
+    selectedField: null, // 选中的田地
     fields: [
       {
         path: "/image/tiandi01.png",
@@ -18,6 +19,22 @@ Page({
         left: 156,
         top: 87,
         id: 2
+      },
+      {
+        path: "/image/tea01.png",
+        width: 100,
+        height: 100,
+        left: 100,
+        top: 300,
+        id: 3
+      },
+      {
+        path: "/image/tea02.png",
+        width: 100,
+        height: 100,
+        left: 201,
+        top: 300,
+        id: 3
       }
     ]
   },
@@ -25,11 +42,36 @@ Page({
   },
   onLoad() {
     this.setData({
-      wWidth: wx.getSystemInfoSync().windowWidth,
-      rateW: 750 / wx.getSystemInfoSync().windowWidth
+      rateW: this.data.cWidth / wx.getSystemInfoSync().windowWidth
     });
   },
-  getImgData(path, offsetLeft, offsetHeight) {
+  pageTapClick(event) {
+    this.setData({
+      selectedField: null
+    });
+    // 获取点击位置相对于页面的位置
+    const pageX = Math.round(this.data.rateW * event.touches[0].pageX);
+    const pageY = Math.round(this.data.rateW * event.touches[0].pageY);
+    // 获取符合该点位置的田地
+    const clickedField = this.data.fields.filter(field => {
+      return field.left <= pageX &&
+        (field.left + field.width) >= pageX &&
+        field.top <= pageY &&
+        (field.top + field.height) >= pageY
+    });
+    if (!clickedField.length) return;
+    // 获取点击的点在田地里的相对位置
+    clickedField.map(field => {
+      this.getImgData(
+        field,
+        Math.round((pageX - field.left)/this.data.rateW),
+        Math.round((pageY - field.top)/this.data.rateW)
+      ).then((selectedField) => {
+        selectedField && !this.data.selectedField && this.dealField(selectedField)
+      })
+    })
+  },
+  getImgData(field, offsetLeft, offsetTop) {
     return new Promise((resolve, reject) => {
       wx.createSelectorQuery()
         .select('#myCanvas')
@@ -41,41 +83,27 @@ Page({
           canvas.width = res[0].width 
           canvas.height = res[0].height
           const image = canvas.createImage()
-          image.src = path
+          image.src = field.path
           // 图片加载完成回调
           image.onload = () => {
+            // 获取田地实际大小
+            const fw = field.width / this.data.rateW;
+            const fh = field.height / this.data.rateW;
             ctx.clearRect(0, 0, canvas.width, canvas.height)
-            ctx.drawImage(image, 0, 0, Math.round(100 / this.data.rateW), Math.round(150 / this.data.rateW))
-            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-            const offsetPosIndex = 4 * (offsetHeight * canvas.width + offsetLeft);
-            resolve(imageData.data[offsetPosIndex] + imageData.data[offsetPosIndex + 1] + imageData.data[offsetPosIndex + 2])
+            ctx.drawImage(image, 0, 0, fw, fh)
+            const imageData = ctx.getImageData(0, 0, fw, fh)
+            const offsetPosIndex = 4 * (offsetTop * fw + offsetLeft) - 1;
+            // 判断是否有像素，确认是否选中图片
+            if (
+              imageData.data[offsetPosIndex - 3] +
+              imageData.data[offsetPosIndex - 2] +
+              imageData.data[offsetPosIndex - 1]
+            ) resolve(field)
           }
       })
     })
   },
-  pageTapClick(event) {
-    // 获取点击位置相对于页面的位置
-    const pageX = this.data.rateW * event.touches[0].pageX;
-    const pageY = this.data.rateW * event.touches[0].pageY;
-    // 获取符合该点位置的田地
-    const clickedField = this.data.fields.filter(field => {
-      return field.left <= pageX &&
-        (field.left + field.width) >= pageX &&
-        field.top <= pageY &&
-        (field.top + field.height) >= pageY
-    });
-    
-    if (!clickedField.length) return;
-    console.log("clickedField", clickedField)
-    // 获取点击的点在田地里的相对位置
-    clickedField.map(field => {
-      this.getImgData(
-        field.path,
-        Math.round((pageX - field.left)/this.data.rateW),
-        Math.round((pageY - field.top)/this.data.rateW)
-      ).then((imgData) => {
-        if (imgData) console.log("field", field)
-      })
-    })
+  dealField(selectedField) {
+    console.log("selectedField", selectedField)
   }
 })
